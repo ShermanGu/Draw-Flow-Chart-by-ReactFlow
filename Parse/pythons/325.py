2ba9b3714b859f372cf02ccb3a80ffe7e5877f0c
ShermanGU <gushuhanjason@sina.com>
2023-06-28 10:28:03 +0800

    deleted examples

--- Parse/pythons/325.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from typing import *
-(black, blue, red, green, yellow, grey, pink, orange, teal, maroon) = range(10)
-
-def get_upper_left_2x2(input_grid: List[List[int]]) -> List[List[int]]:
-    return [row[:2] for row in input_grid[:2]]
-
-def main(input_grid):
-    return get_upper_left_2x2(input_grid)
